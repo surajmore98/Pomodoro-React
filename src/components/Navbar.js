9c0f87e9a0ceb6d19b345b1d3ae2276388288fd6
suraj more <29055479+surajmore98@users.noreply.github.com>
2022-03-28 20:18:04 +0530

    #fix - removed commented code and changed font-size value.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,6 @@ function Navbar() {
                 <div className="logo pl-xl">
                     <Link to="/" className="logo-text charcoal-white">Pomodoro</Link>
                 </div>
-                {/* <ul className="nav-action ml-auto mr-md">
-                    <li className="nav-action-item">
-                        <a className="nav-action-text charcoal-white mr-md" href="./alert.html">Components</a></li>
-                    <li className="nav-action-item"><a className="nav-action-text charcoal-white" href="https://github.com/surajmore98/charcoal-ui">Github</a></li>
-                </ul> */}
             </nav>
         </div>
     )
